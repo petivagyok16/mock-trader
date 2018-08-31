d5be451152cef937ca417516c60357101c48aa83
peter palocz <peter.palocz91@gmail.com>
2018-08-31 19:11:07 +0200

    feat: fix CloudAMPQ connection

--- src/main/java/com/robotrade/mocktrader/rabbitMQ/TransactionConfig.java
+++ src/main/java/com/robotrade/mocktrader/rabbitMQ/TransactionConfig.java
@@ -1,7 +1,6 @@
 package com.robotrade.mocktrader.rabbitMQ;
 
 import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,18 +15,6 @@ public class TransactionConfig {
 	@Bean
 	public TransactionSender transactionSender() {
 		return new TransactionSender();
-	}
-
-	@Bean
-	public void setupHerokuRabbitConnection() {
-		CachingConnectionFactory connectionFactory=new CachingConnectionFactory("wolverine-01.rmq.cloudamqp.com");
-		connectionFactory.setUsername("jkkhhmwu");
-		connectionFactory.setPassword("6rLuxEU_z6N6VTMgJggcEL_qMpaW6B2S");
-		connectionFactory.setVirtualHost("jkkhhmwu");
-
-		// Recommended settings
-		connectionFactory.setRequestedHeartBeat(30);
-		connectionFactory.setConnectionTimeout(30000);
 	}
 
 }
