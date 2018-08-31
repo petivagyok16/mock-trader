03d2dd92f21215db2ccba9385624769069f54da2
peter palocz <peter.palocz91@gmail.com>
2018-08-31 19:05:55 +0200

    feat: configure CloudAMPQ connection

--- src/main/java/com/robotrade/mocktrader/rabbitMQ/TransactionConfig.java
+++ src/main/java/com/robotrade/mocktrader/rabbitMQ/TransactionConfig.java
@@ -1,6 +1,7 @@
 package com.robotrade.mocktrader.rabbitMQ;
 
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +16,18 @@ public class TransactionConfig {
 	@Bean
 	public TransactionSender transactionSender() {
 		return new TransactionSender();
+	}
+
+	@Bean
+	public void setupHerokuRabbitConnection() {
+		CachingConnectionFactory connectionFactory=new CachingConnectionFactory("wolverine-01.rmq.cloudamqp.com");
+		connectionFactory.setUsername("jkkhhmwu");
+		connectionFactory.setPassword("6rLuxEU_z6N6VTMgJggcEL_qMpaW6B2S");
+		connectionFactory.setVirtualHost("jkkhhmwu");
+
+		// Recommended settings
+		connectionFactory.setRequestedHeartBeat(30);
+		connectionFactory.setConnectionTimeout(30000);
 	}
 
 }
