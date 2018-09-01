5f45073417d024df683df5eecbb1c0c8cec67619
peter palocz <peter.palocz91@gmail.com>
2018-09-01 19:51:46 +0200

    trying to configure cloud AMPQ

--- src/main/java/com/robotrade/mocktrader/rabbitMQ/TransactionHistory/TransactionHistoryConfig.java
+++ src/main/java/com/robotrade/mocktrader/rabbitMQ/TransactionHistory/TransactionHistoryConfig.java
@@ -11,30 +11,30 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class TransactionHistoryConfig {
 
-//	public static class ServerConfig {
-//
-//		@Bean
-//		public Queue queue() {
-//			return new Queue(RabbitConstants.TRANSACTION_HISTORY_QUEUE_REQUEST_NAME);
-//		}
-//
-//		@Bean
-//		public DirectExchange exchange() {
-//			return new DirectExchange(RabbitConstants.TRANSACTION_HISTORY_EXCHANGE_NAME);
-//		}
-//
-//		@Bean
-//		public Binding binding(DirectExchange exchange,
-//													 Queue queue) {
-//			return BindingBuilder.bind(queue)
-//							.to(exchange)
-//							.with(RabbitConstants.TRANSACTION_HISTORY_REQUEST_ROUTING_KEY);
-//		}
-//
-//		@Bean
-//		public TransactionHistoryServer server() {
-//			return new TransactionHistoryServer();
-//		}
-//	}
+	public static class ServerConfig {
+
+		@Bean
+		public Queue queue() {
+			return new Queue(RabbitConstants.TRANSACTION_HISTORY_QUEUE_REQUEST_NAME);
+		}
+
+		@Bean
+		public DirectExchange exchange() {
+			return new DirectExchange(RabbitConstants.TRANSACTION_HISTORY_EXCHANGE_NAME);
+		}
+
+		@Bean
+		public Binding binding(DirectExchange exchange,
+													 Queue queue) {
+			return BindingBuilder.bind(queue)
+							.to(exchange)
+							.with(RabbitConstants.TRANSACTION_HISTORY_REQUEST_ROUTING_KEY);
+		}
+
+		@Bean
+		public TransactionHistoryServer server() {
+			return new TransactionHistoryServer();
+		}
+	}
 
 }
