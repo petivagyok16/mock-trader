0e611551484ef1a24476ae459ec37e29e699bd44
peter palocz <peter.palocz91@gmail.com>
2018-09-02 20:12:59 +0200

    rename constants

--- src/main/java/com/robotrade/mocktrader/rabbitMQ/RabbitConstants.java
+++ src/main/java/com/robotrade/mocktrader/rabbitMQ/RabbitConstants.java
@@ -4,9 +4,9 @@ public class RabbitConstants {
 	public static final String TRANSACTION_ROUTING_KEY = "roboTrade.transaction";
 	public static final String ROBO_TRANSACTION_EXCHANGE_NAME = "robo-transaction-exchange";
 
-	public static final String TRANSACTION_HISTORY_REQUEST_ROUTING_KEY = "roboTransactionHistoryRequest";
-	public static final String TRANSACTION_HISTORY_EXCHANGE_NAME = "roboTransactionHistoryExchange";
-	public static final String TRANSACTION_HISTORY_QUEUE_REQUEST_NAME = "roboTransactionHistoryRequest";
+	public static final String TRANSACTION_HISTORY_REQUEST_ROUTING_KEY = "roboTrade.transaction-history-request";
+	public static final String TRANSACTION_HISTORY_EXCHANGE_NAME = "robo-transaction-history-exchange";
+	public static final String TRANSACTION_HISTORY_QUEUE_REQUEST_NAME = "robo-transaction-history-queue";
 
 	public static final String USER_CAPITAL_REQUEST_ROUTING_KEY = "roboTrade.users-capital";
 	public static final String USER_CAPITAL_EXCHANGE_NAME= "robo-users-capital-exchange";
