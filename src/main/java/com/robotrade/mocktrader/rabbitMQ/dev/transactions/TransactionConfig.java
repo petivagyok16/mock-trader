93c3832bca85fc30131842f6ecbc10d86a9ea53d
peter palocz <peter.palocz91@gmail.com>
2018-12-26 13:39:08 +0100

    fix(TransactionConfig): fix import

--- src/main/java/com/robotrade/mocktrader/rabbitMQ/dev/transactions/TransactionConfig.java
+++ src/main/java/com/robotrade/mocktrader/rabbitMQ/dev/transactions/TransactionConfig.java
@@ -1,7 +1,6 @@
 package com.robotrade.mocktrader.rabbitMQ.dev.transactions;
 
 import com.robotrade.mocktrader.rabbitMQ.RabbitConstants;
-import com.robotrade.mocktrader.rabbitMQ.prod.transactions.TransactionSender;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
