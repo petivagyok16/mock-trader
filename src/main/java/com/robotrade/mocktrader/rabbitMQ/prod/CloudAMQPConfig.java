20c9e4f7e933a6c13d2064348e0d583b608cebfe
peter palocz <peter.palocz91@gmail.com>
2018-12-30 18:14:21 +0100

    delete unusued import

--- src/main/java/com/robotrade/mocktrader/rabbitMQ/prod/CloudAMQPConfig.java
+++ src/main/java/com/robotrade/mocktrader/rabbitMQ/prod/CloudAMQPConfig.java
@@ -7,7 +7,6 @@ import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
