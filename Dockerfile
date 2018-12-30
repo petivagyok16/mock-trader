63b1f4e7a640862f604ac617df54c58edd91dd64
peter palocz <peter.palocz91@gmail.com>
2018-12-30 16:01:47 +0100

    refactor(App): refactor to env variables

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,17 @@
-FROM java:8
+FROM maven:3.6.0-jdk-8-alpine as builder
 WORKDIR './app'
-COPY ./target/mock-trader-0.0.1-SNAPSHOT.jar ./
+
+# add pom.xml only here, and download dependency
+ADD pom.xml .
+
+RUN ["/usr/local/bin/mvn-entrypoint.sh", "mvn", "verify", "clean", "--fail-never"]
+
+# now we can add all source code and start compiling
+ADD . .
+
+RUN ["mvn", "package"]
+
+
+FROM openjdk:8-jre
+COPY --from=builder ./app/target/mock-trader-0.0.1-SNAPSHOT.jar ./
 ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","mock-trader-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/robotrade/mocktrader/rabbitMQ/prod/CloudAMQPConfig.java
+++ src/main/java/com/robotrade/mocktrader/rabbitMQ/prod/CloudAMQPConfig.java
@@ -1,11 +1,11 @@
 package com.robotrade.mocktrader.rabbitMQ.prod;
 
-import com.robotrade.mocktrader.rabbitMQ.prod.common.CloudAMPQCredentials;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -15,13 +15,26 @@ import org.springframework.context.annotation.Profile;
 @Profile("prod")
 public class CloudAMQPConfig {
 
+	@Value("${CLOUDAMQP_HOSTNAME}")
+	private String hostName;
+
+	@Value("${CLOUDAMQP_PASSWORD}")
+	private String password;
+
+	@Value("${CLOUDAMQP_USERNAME}")
+	private String username;
+
+	@Value("${CLOUDAMQP_VIRTUALHOST}")
+	private String virtualhost;
+
+
 	private final CachingConnectionFactory cloudAMPQConnection;
 
 	public CloudAMQPConfig() {
-		CachingConnectionFactory connectionFactory = new CachingConnectionFactory(CloudAMPQCredentials.HOSTNAME);
-		connectionFactory.setUsername(CloudAMPQCredentials.USERNAME);
-		connectionFactory.setPassword(CloudAMPQCredentials.PASSWORD);
-		connectionFactory.setVirtualHost(CloudAMPQCredentials.VIRTUAL_HOST);
+		CachingConnectionFactory connectionFactory = new CachingConnectionFactory(this.hostName);
+		connectionFactory.setUsername(this.username);
+		connectionFactory.setPassword(this.password);
+		connectionFactory.setVirtualHost(this.virtualhost);
 
 		// Recommended settings
 		connectionFactory.setRequestedHeartBeat(30);
